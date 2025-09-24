7eef509e57ee8f23236f31321745c002b25d2c96
Yusha254 <Kashikeishaz123!>
2025-09-24 16:27:42 +0300

    Project Folder

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
+
+interface PageRouteProps {
+  pageRoute: string;
+}
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Splash App</h1>
+    </div>
+  );
+};
+
+export default Home;
